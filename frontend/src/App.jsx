ce1a6634f663937b6cd6985d80791085472c09f5
Trilok Lowanshi <trilokl01c@gmail.com>
2026-07-31 20:19:10 +0530

    Added fetching in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-
+const API = "https://localhost:";
 function App() {
+    const [message, setMsg] = useState("");
+
+    useEffect(() => {
+        fetch(API)
+            .then((res) => JSON(res))
+            .then((data) => setMsg(data.message))
+            .catch((err) => console.error("Error fetching from API:", err));
+    }, []);
     return (
         <div className="App">
             <h3>See what server is sending here</h3>
+            <p>{message || "Loading..."}</p>
         </div>
     );
 }
