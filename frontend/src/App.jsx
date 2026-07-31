dba32ae1256f07996b1fbfca008a2ef3c4264dd4
Trilok Lowanshi <trilokl01c@gmail.com>
2026-07-31 20:09:32 +0530

    removed unnecessary things

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,9 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const colors = ["#53d860", "#d7d356", "#903939", "#e3a283"];
-    const [bgColor, setBG] = useState("#903939");
-
-    useEffect(() => {
-        setBG(colors[Math.floor(Math.random() * 4)]);
-    }, []);
     return (
-        <div className="App" style={{ backgroundColor: bgColor }}>
+        <div className="App">
             <h3>See what server is sending here</h3>
-            <button
-                onClick={() => {
-                    setCount(count + 1);
-                }}
-            >
-                Hit me!
-            </button>
-            <p className="counts">{count}</p>
         </div>
     );
 }
